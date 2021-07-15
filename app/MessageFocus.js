cb6c7e1396f9962e8dd19cef8fafd0cf3876976a
Jay Vasquez <john.joseph.vasquez@codeandtheory.com>
2021-07-15 09:16:41 +0800

    Update messagefocus API to accept custom fields

--- app/MessageFocus.js
+++ app/MessageFocus.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 const instance = null;
+const adestra_details = {
+    table_id = '',
+    campaign_id = '',
+    list_id = ''
+};
 
 class MessageFocus{
     constructor() {
@@ -14,18 +19,21 @@ class MessageFocus{
 
     async start(req) {
         let body = JSON.parse(req['data.json']);
-        console.log(body);
-        // let newContact = await this.createContact(body);
-        // await this.addList(newContact);
-        // let response = await this.sendSingle(newContact);
-        // return response;
+        this.adestra_details.table_id = body.table_id[0];
+        this.adestra_details.campaign_id = body.campaign_id[0];
+        this.adestra_details.list_id = body.list_id[0];
+        
+        let newContact = await this.createContact(body);
+        await this.addList(newContact);
+        let response = await this.sendSingle(newContact);
+        return response;
     }
 
     async createContact(body) {
         let response = null;
         try{
             response = await this.instance.post(`/contacts`, {
-                table_id: parseInt(process.env.ADESTRA_TABLE_ID),
+                table_id: parseInt(this.adestra_details.table_id),
                 contact_data: {
                     email: body.email[0],
                     first_name: body.first_name[0],
@@ -42,7 +50,7 @@ class MessageFocus{
     async addList(contact) {
         let response = null;
         try{
-            response = await this.instance.post(`/contacts/${contact.id}/lists/${process.env.ADESTRA_LIST_ID}`)
+            response = await this.instance.post(`/contacts/${contact.id}/lists/${this.adestra_details.list_id}`)
         } catch (err) {
             response = { data: err.response.data };
         }
@@ -53,7 +61,7 @@ class MessageFocus{
     async sendSingle(contact) {
         let response = null;
         try{
-            response = await this.instance.post(`/campaigns/${process.env.ADESTRA_CAMPAIGN_ID}/send_single`, {
+            response = await this.instance.post(`/campaigns/${this.adestra_details.campaign_id}/send_single`, {
                 send_single_contact: contact.id,
                 send_single_options: {
                     suppression_info: true
